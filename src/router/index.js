51faa615c19124f60550a9c1900ea128aecada7f
hua <miku071522@gmail.com>
2024-11-19 16:38:10 +0800

    add daily-card

--- src/router/index.js
+++ src/router/index.js
@@ -8,19 +8,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
       path: "/card-info",
       name: "card-info",
       component: CardInfo,
@@ -35,6 +22,11 @@ const router = createRouter({
       name: "hero-member",
       component: HeroMemberView,
     },
+    {
+      path: '/daily-card',
+      name: 'daily-info',
+      component: () => import('../views/DailyCard.vue')
+    }
   ],
 });
 

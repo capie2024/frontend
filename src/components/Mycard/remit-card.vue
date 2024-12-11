fb274651c98d91673116a3d3a96bb650bedcad33
Tales <allen84324@gmail.com>
2024-12-11 15:37:55 +0800

    更新css

--- src/components/Mycard/remit-card.vue
+++ src/components/Mycard/remit-card.vue
@@ -42,20 +42,6 @@
                             </svg>
                             <span data-v-41768621="" class="text-sm font-mono">匯出牌組</span>
                         </button>
-                        <!-- <div data-v-41768621="" class="relative  z-20  max-h-[50vh] md:max-h-[20rem] overflow-y-auto flex flex-col rounded-md bg-zinc-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none max-w-[80vw] md:max-w-[20rem] w-max" style="margin-top: 48px; left: 0px; top: 0px;">
-                            <a class="text-sm cursor-pointer flex items-center gap-2 py-2 px-4 text-zinc-300 hover:text-white">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="size-5 flex-none">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"></path>
-                                </svg><span>{{ isSecondButtonVisible}}匯出牌組</span>
-                            </a>
-                            <a class=" text-sm cursor-pointer flex items-center gap-2 py-2 px-4 text-zinc-300 hover:text-white">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="size-5 flex-none">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 0 1-1.161.886l-.143.048a1.107 1.107 0 0 0-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 0 1-1.652.928l-.679-.906a1.125 1.125 0 0 0-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 0 0-8.862 12.872M12.75 3.031a9 9 0 0 1 6.69 14.036m0 0-.177-.529A2.25 2.25 0 0 0 17.128 15H16.5l-.324-.324a1.453 1.453 0 0 0-2.328.377l-.036.073a1.586 1.586 0 0 1-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 0 1-5.276 3.67m0 0a9 9 0 0 1-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25">
-
-                                        </path>
-                                    </svg><span>{{ isSecondButtonVisible}}匯出文章</span>
-                                </a>
-                            </div> -->
 
                         <div data-v-41768621="" class="input-item-2 ">
                             <input data-v-41768621="" class="input-text" type="text" placeholder="代碼">
@@ -93,7 +79,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"> 
                             </path>
                             </svg>
-                                <span class="text-sm font-mono">牌組 pdf</span>
+                                <span class="text-sm font-mono">PDF</span>
                         </button>
                      
                     </div>
@@ -107,12 +93,12 @@
                                 </svg>
                                 <span class="text-sm font-mono">A4</span>
                             </button>
-                            <button  class="item default-transition active button-remit-div">
+                            <!-- <button  class="item default-transition active button-remit-div">
                                 <svg data-v-41768621="" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="icon size-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"></path>
                                 </svg>
                                 <span class="text-sm font-mono">A4 (7-11 ibon)</span>
-                            </button>
+                            </button> -->
                         </div>
                         
                     </div>
@@ -402,8 +388,6 @@
         display: flex;
         gap: .5rem;
         padding: .5rem;
-        border: 1px solid #27272a;
-        background-color: #151516;
         cursor: pointer;
     }
     

2fb128a9d9394ae42c932b636d8bdb5d7e99041c
呂旻叡 <minjuilu@lvminruideMacBook-Air.local>
2024-12-15 15:43:41 +0800

    update

--- src/components/card-deck/Carddeck.vue
+++ src/components/card-deck/Carddeck.vue
@@ -34,6 +34,8 @@ export default {
         sortedCards: [],
         sortBy: "typeTranslate",
         groupedCards: [],
+        toggleTableView: false,
+        togglePriceView: false
         };
     },
     mounted() {
@@ -115,6 +117,12 @@ export default {
         },    
     },
     methods: {
+        togglePriceTableView() {
+            this.togglePriceView = !this.togglePriceView;
+        },
+        toggleViewMode() {
+            this.toggleTableView = !this.toggleTableView;
+        },
         countSoulCards(cards) {
             return cards.filter(card => card.trigger.includes('soul')).length;
         },
@@ -126,11 +134,8 @@ export default {
                 const postCode = this.$route.params.post_code;  // 获取当前路由的 post_code
                 const response = await axios.get(`http://localhost:3000/api/deck/${postCode}`);
 
-                // 遍历所有 deck_list，获取每个 deck 的信息
                 const deckList = response.data[0].deck_list;
-                this.deckName = deckList.deck_name;  // 获取 deck_name
-                this.cards = deckList.deck;  // 获取所有 deck 中的卡片
-                console.log(this.cards);
+                this.cards = deckList.deck;  
 
                 console.log('Deck Name:', this.deckName);
                 console.log('All cards:', this.cards);
@@ -758,15 +763,18 @@ export default {
                             </button>
                         </div>
                         <div class="toolbar-area2">
-                            <button class="func-btn func-btn3">
-                                <svg data-v-5634e853="" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"></path></svg>
-                                <div class="func-text func-text3">開啟價格</div>
+                            <button class="func-btn func-btn4" @click="togglePriceTableView">
+                                <svg data-v-5634e853="" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"></path>
+                                </svg>
+                                <div class="func-text func-text4">開啟價格</div>
                             </button>
-                            <button class="func-btn func-btn5">
+                            <button class="func-btn func-btn5" @click="toggleTableView = true"
+                            :style="{ backgroundColor: toggleTableView ? '#06b6d4' : '' }">
                                 <svg data-v-5634e853="" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"></path></svg>
                                 <div class="func-text func-text5">表格顯示</div>
                             </button>
-                            <button class="func-btn func-btn6">
+                            <button class="func-btn func-btn6" @click="toggleTableView = false"
+                            :style="{ backgroundColor: !toggleTableView ? '#06b6d4' : '' }">
                                 <svg data-v-5634e853="" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9"></path></svg>
                                 <div class="func-text func-text6">卡片資訊</div>
                             </button>
@@ -784,6 +792,14 @@ export default {
                             </div>
                             <div class="card-row">
                                 <div class="col-Info" v-for="card in group.cards" :key="card.index">
+                                    <p class="price-row" v-if="!togglePriceView">
+                                        <svg data-v-27aeb5c1="" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="currency-icon">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m9 7.5 3 4.5m0 0 3-4.5M12 12v5.25M15 12H9m6 3H9m12-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                                        </svg>
+                                        <span data-v-27aeb5c1="" class="price-row-text">{{ card.price.number }}</span>
+                                        <span data-v-27aeb5c1="" class="price-row-text">{{ card.rare }}</span>
+                                    </p>
+
                                     <div class="card-info-image">
                                         <img :src="card.cover">
                                         <div class="card-inner-info">
@@ -792,14 +808,14 @@ export default {
                                                 <p>{{ card.rare }}</p>
                                             </div>
                                             <h3>{{ card.title }}</h3>
-                                            <div class="details">
+                                            <div class="details" v-if="!toggleTableView">
                                                 <div><span>類型</span>{{ card.typeTranslate }}</div>
                                                 <div><span>魂傷</span>{{ card.soul }}</div>
                                                 <div><span>等級</span>{{ card.level }}</div>
                                                 <div><span>攻擊</span>{{ card.attack }}</div>
                                                 <div><span>費用</span>{{ card.cost }}</div>
                                             </div>
-                                            <div class="price-download">
+                                            <div class="price-download" v-if="!toggleTableView">
                                                 <p> ${{ card.price.number }} </p>
                                             <button><svg data-v-69cfbdbc="" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="size-7 text-white stroke-2"><path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3"></path></svg></button>
                                             </div>
@@ -865,6 +881,23 @@ export default {
 </template>
 
 <style scoped>
+    .price-row{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: 700;
+        color: #c9a83d;
+        gap: .25rem;
+        margin-top: 30px;
+        font-size: 16px;
+    }
+
+    .currency-icon{
+        width: 20px;
+        height: 20px;
+        stroke: #c9a83d;
+    }
+
     .tool-btn1.active{
         background: linear-gradient(45deg, #7dca31, #21b8c8);
     }
@@ -2072,6 +2105,7 @@ export default {
         color: white;
         font-size: 14px;
         font-weight: 900;
+        width: 100%;
     }
 
     .tool-btn1:hover {
@@ -2120,8 +2154,8 @@ export default {
         visibility: hidden;
         transition:ease 0.3s;
         position:absolute;
-        right:0;
-        top:35px;
+        right: 20px;
+        top: 50px;
     }
 
     .toolbar-area2 {
@@ -2151,11 +2185,11 @@ export default {
     }
 
     .func-text5 {
-        right:-33px;
+        right:-30px;
     }
 
     .func-text6 {
-        right:-35px;
+        right:-25px;
     }
 
     .func-btn1:hover .func-text1,
@@ -2350,6 +2384,19 @@ export default {
     }
 
     @media screen and (max-width: 1199px) {
+        .toolbar {
+            position: absolute;
+            top: 930px;
+        }
+
+        .card-info{
+            margin-top: 400px;
+        }
+
+        .tool-btn1 span{
+            width: 100%;
+        }
+
         .col-Sheet, .col-Info {
             width: calc((100% - 10px) / 3);
         }
@@ -2536,8 +2583,7 @@ export default {
         }
 
         .footer-nav {
-            /* display: flex; */
-            display: none;    
+            display: flex;
         }
 
         .deck-container {
@@ -2551,21 +2597,16 @@ export default {
 
     @media screen and (max-width: 800px) {
         .toolbar {
-            width: 50%;
-            flex-direction: column;
-            /* display: none; */
+            width: 100%;
         }
         
         .toolbar-area1 {
-            width: 50%;
             gap: 8px;
-            /* display: none; */
         }
         
         .toolbar-area2 {
-            width: 50%;
-            position: static;
-            /* display: none; */
+            gap: 8px;
+
         } 
     }
 

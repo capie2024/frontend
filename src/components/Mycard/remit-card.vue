c5c62e31409a331bf7e6ebdf335440f5024afbc7
Tales <allen84324@gmail.com>
2024-12-18 16:27:39 +0800

    順便修改OfficialProductList Sidebar

--- src/components/Mycard/remit-card.vue
+++ src/components/Mycard/remit-card.vue
@@ -80,18 +80,6 @@
                                 <span class="text-sm font-mono">PDF</span>
                         </button>
                     </div>
-    
-                    <!-- <div class="modal-body">
-                        <h3 data-v-41768621="" class="topic my-4"> 選擇紙張大小</h3>
-                        <div data-v-41768621="" class="grid grid-cols-2 gap-2">
-                            <button  class="item default-transition active button-remit-div  focus-button" id="button-remit-div"  :class="{ active: buttons[2] }" @click="toggleActive(2)">
-                                <svg data-v-41768621="" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="icon size-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"></path>
-                                </svg>
-                                <span class="text-sm font-mono">A4</span>
-                            </button>
-                        </div>
-                    </div> -->
 
                     <div data-v-c3ac02c2="" class="seeting">
                                 <button  class="item  justify-center default-transition gray setting-text" id="setting-text" @click="exportToPDF" :disabled="!allButtonsActive"

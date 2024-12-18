1ed8b825543b6f2c92478278294e00fff753bd92
hua <miku071522@gmail.com>
2024-12-18 16:12:47 +0800

    import sidebar grid to daily card

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,3 @@
-<!-- <script setup>
-import { ref, onMounted } from "vue";
-import { RouterLink, RouterView } from "vue-router";
-// import CardInfo from "@/views/CardInfo.vue";
-import { storeToRefs } from "pinia";
-import { useCardInfoStore } from "@/stores/card-info";
-
-const cardInfoStore = useCardInfoStore();
-const { cardInfoDisplay } = storeToRefs(cardInfoStore);
-</script> -->
-
-<template>
+<template> 
     <router-view />
 </template>
-
-<style scoped>
-
-</style>

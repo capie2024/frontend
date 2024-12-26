bfd5e41568cdb6636d1ae02684c5fd8a4a324635
Tales <allen84324@gmail.com>
2024-12-26 13:54:31 +0800

    去除svg多餘的 data class

--- src/views/Card List by Series.vue
+++ src/views/Card List by Series.vue
@@ -50,7 +50,6 @@
               </button>
             </div>
           </div>
-
                             <div class="icons">
                                 <notice/>
                                 <NavLoginBtn/>
@@ -65,7 +64,7 @@
                                     </div>
                                     <div class="card-text">
                                         <div class="flex">
-                                            <svg data-v-09f2b439="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon" class="flex-none icon-size color-a1">
+                                            <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon" class="flex-none icon-size color-a1">
                                                 <path d="M16.5 6a3 3 0 0 0-3-3H6a3 3 0 0 0-3 3v7.5a3 3 0 0 0 3 3v-6A4.5 4.5 0 0 1 10.5 6h6Z">
                                                 </path>
                                                 <path d="M18 7.5a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-7.5a3 3 0 0 1-3-3v-7.5a3 3 0 0 1 3-3H18Z">
@@ -89,7 +88,7 @@
                                     </div>
                                     <div class="card-text">
                                         <div class="flex">
-                                            <svg data-v-09f2b439="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon" class="flex-none icon-size color-a1">
+                                            <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon" class="flex-none icon-size color-a1">
                                                 <path d="M16.5 6a3 3 0 0 0-3-3H6a3 3 0 0 0-3 3v7.5a3 3 0 0 0 3 3v-6A4.5 4.5 0 0 1 10.5 6h6Z">
                                                 </path>
                                                 <path d="M18 7.5a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-7.5a3 3 0 0 1-3-3v-7.5a3 3 0 0 1 3-3H18Z">
@@ -105,6 +104,7 @@
                     </div>
                     <MainFooter/>
                 </div>
+              </div>
 </template>
 
 <script setup>
